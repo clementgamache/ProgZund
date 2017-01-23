94b04a8e2e5c29a6680dd3fbd173dd2c132608df
Clement Gamache <clement.gamache@polymtl.ca>
2017-01-23 11:41:48 -0500

    added a turtle window in which the zund is simulated

---
+++ C#/ProgZund/ProgZund/TurtleSimulationForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ProgZund
+{
+    partial class TurtleSimulationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClose.Location = new System.Drawing.Point(12, 41);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(12, 12);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play Simulation";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // TurtleSimulationForm
+            // 
+            this.AcceptButton = this.buttonPlay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonClose;
+            this.ClientSize = new System.Drawing.Size(536, 301);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.buttonClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TurtleSimulationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TurtleSimulationForm";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.TurtleSimulationForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonPlay;
+    }
+}
---
+++ C#/ProgZund/ProgZund/TurtleSimulationForm.cs
@@ -0,0 +1,81 @@
+﻿using Nakov.TurtleGraphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Shapes;
+
+namespace ProgZund
+{
+    public partial class TurtleSimulationForm : Form
+    {
+        private List<PolyLine> polylines;
+
+        public TurtleSimulationForm(List<PolyLine> polylines)
+        {
+            InitializeComponent();
+            this.polylines = polylines;
+        }
+
+        private void TurtleSimulationForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Turtle.Dispose();
+            this.Close();
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            float minX, minY, maxX, maxY;
+            minX = -400;
+            minY = -250;
+            maxX = 500;
+            maxY = 250;
+            System.Windows.Size size = PolyLine.getSize(polylines);
+            System.Windows.Point position = PolyLine.getPosition(polylines);
+            float scale;
+            Turtle.Delay = 1;
+            if (size.Width/size.Height > (maxX - minX)/(maxY-minY))
+            {
+                scale = (float)size.Width / (maxX - minX);
+            }
+            else
+            {
+                scale = (float)size.Height / (maxY - minY);
+            }
+            Turtle.PenSize = 2;
+            Turtle.PenColor = Color.Green;
+            Turtle.PenUp();
+            Turtle.ShowTurtle = true;
+            try
+            {
+
+                foreach (PolyLine polyline in polylines)
+                {
+                    List<Line> lines = polyline.getLines();
+                    Turtle.MoveTo((float)lines[0].X1 / scale + (float)position.X * scale + minX, (float)lines[0].Y1 / scale + (float)position.Y * scale + minY);
+                    Turtle.PenDown();
+                    foreach (Line l in lines)
+                    {
+                        //Turtle.MoveTo((float)l.X1 / scale + (float)position.X * scale + minX, (float)l.Y1 / scale + (float)position.Y * scale +minY);
+                        Turtle.MoveTo((float)l.X2 / scale + (float)position.X * scale + minX, (float)l.Y2 / scale + (float)position.Y * scale + minY);
+                    }
+                    Turtle.PenUp();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
